ac4e45cd9239ab709933e8476c5ccce3285fdc68
Piotr Oleś <piotrek.oles@gmail.com>
2020-05-10 09:55:59 +0200

    fix: make definition files compatible with IDEs

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -19,84 +19,76 @@ import {
   ExpressionNode,
 } from "@rsql/ast";
 
-function comparison(
-  selector: string,
-  operator: ComparisonOperator,
-  value: string | number | (string | number)[]
-): ComparisonNode {
-  return createComparisonNode(
-    createSelectorNode(selector),
-    operator,
-    createValueNode(Array.isArray(value) ? value.map((singleValue) => String(singleValue)) : String(value))
-  );
+interface Builder {
+  comparison(
+    selector: string,
+    operator: ComparisonOperator,
+    value: string | number | (string | number)[]
+  ): ComparisonNode;
+  eq(selector: string, value: string | number): ComparisonNode;
+  neq(selector: string, value: string | number): ComparisonNode;
+  le(selector: string, value: string | number): ComparisonNode;
+  lt(selector: string, value: string | number): ComparisonNode;
+  ge(selector: string, value: string | number): ComparisonNode;
+  gt(selector: string, value: string | number): ComparisonNode;
+  in(selector: string, values: (string | number)[]): ComparisonNode;
+  out(selector: string, values: (string | number)[]): ComparisonNode;
+  logic(expressions: ExpressionNode[], operator: LogicOperator): ExpressionNode;
+  and(...expressions: ExpressionNode[]): ExpressionNode;
+  or(...expressions: ExpressionNode[]): ExpressionNode;
 }
 
-function eq(selector: string, value: string | number): ComparisonNode {
-  return comparison(selector, EQ, value);
-}
-
-function neq(selector: string, value: string | number): ComparisonNode {
-  return comparison(selector, NEQ, value);
-}
-
-function le(selector: string, value: string | number): ComparisonNode {
-  return comparison(selector, LE, value);
-}
-
-function lt(selector: string, value: string | number): ComparisonNode {
-  return comparison(selector, LT, value);
-}
-
-function ge(selector: string, value: string | number): ComparisonNode {
-  return comparison(selector, GE, value);
-}
-
-function gt(selector: string, value: string | number): ComparisonNode {
-  return comparison(selector, GT, value);
-}
-
-function in_(selector: string, values: (string | number)[]): ComparisonNode {
-  return comparison(selector, IN, values);
-}
-
-function out(selector: string, values: (string | number)[]): ComparisonNode {
-  return comparison(selector, OUT, values);
-}
-
-function logic(expressions: ExpressionNode[], operator: LogicOperator): ExpressionNode {
-  if (!expressions.length) {
-    throw new Error(`The logic expression builder requires at least one expression but none passed.`);
-  }
-
-  return expressions
-    .slice(1)
-    .reduce(
-      (leftExpression, rightExpression) => createLogicNode(leftExpression, operator, rightExpression),
-      expressions[0]
+const builder: Builder = {
+  comparison(selector, operator, value) {
+    return createComparisonNode(
+      createSelectorNode(selector),
+      operator,
+      createValueNode(Array.isArray(value) ? value.map((singleValue) => String(singleValue)) : String(value))
     );
-}
+  },
+  eq(selector, value) {
+    return builder.comparison(selector, EQ, value);
+  },
+  neq(selector, value) {
+    return builder.comparison(selector, NEQ, value);
+  },
+  le(selector, value) {
+    return builder.comparison(selector, LE, value);
+  },
+  lt(selector, value) {
+    return builder.comparison(selector, LT, value);
+  },
+  ge(selector, value) {
+    return builder.comparison(selector, GE, value);
+  },
+  gt(selector, value) {
+    return builder.comparison(selector, GT, value);
+  },
+  in(selector, values) {
+    return builder.comparison(selector, IN, values);
+  },
+  out(selector, values) {
+    return builder.comparison(selector, OUT, values);
+  },
+  logic(expressions, operator) {
+    if (!expressions.length) {
+      throw new Error(`The logic expression builder requires at least one expression but none passed.`);
+    }
 
-function and(...expressions: ExpressionNode[]): ExpressionNode {
-  return logic(expressions, AND);
-}
-
-function or(...expressions: ExpressionNode[]): ExpressionNode {
-  return logic(expressions, OR);
-}
-
-const builder = {
-  comparison,
-  eq,
-  neq,
-  le,
-  lt,
-  ge,
-  gt,
-  in: in_,
-  out,
-  logic,
-  and,
-  or,
+    return expressions
+      .slice(1)
+      .reduce(
+        (leftExpression, rightExpression) => createLogicNode(leftExpression, operator, rightExpression),
+        expressions[0]
+      );
+  },
+  and(...expressions) {
+    return builder.logic(expressions, AND);
+  },
+  or(...expressions) {
+    return builder.logic(expressions, OR);
+  },
 };
 
 export default builder;
+export { Builder };
